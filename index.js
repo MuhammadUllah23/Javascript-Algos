d6f78109854bc38475781cdb2db940d00fb1c31f
Muhammad Ullah <78986038+MuhammadUllah23@users.noreply.github.com>
2022-08-07 19:33:06 +0000

    Write Radix Sort pseudocode

--- index.js
+++ index.js
@@ -431,3 +431,14 @@ function mostDigits(nums) {
 
 // -------------------------------------------------------------
 
+// RADIX SORT
+// Pseudocode
+// Define a function that accepts a list of numbers.
+// Figure out how many digits the largest number has.
+// Loop from k=0 up to the largest numbers of digits.
+// For each iteration create buckets for each digit (0-9) and place each number in the corresponding bucket based on its kth digit.
+// Replace our existing array with values in our buckets, starting with 0 and going up to 9.
+// Return list.
+
+
+
