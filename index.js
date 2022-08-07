ea2e7d46a6175e81721ef07bf2523fcc979b1829
Muhammad Ullah <78986038+MuhammadUllah23@users.noreply.github.com>
2022-08-07 19:20:33 +0000

    create digitCount function. Radix Sorter Helper

--- index.js
+++ index.js
@@ -414,3 +414,9 @@ function getDigit(num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
+function digitCount(num) {
+  // returns the number of digits in num
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
