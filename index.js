e63657e29f4026270191ae06bb7e093398995a15
Muhammad Ullah <78986038+MuhammadUllah23@users.noreply.github.com>
2022-08-23 06:06:53 +0000

    practice writing merge sort algorithm

--- index.js
+++ index.js
@@ -344,34 +344,8 @@ function mergeArrays(arr1, arr2) {
   return result
 }
 
-function practiceMergeArrays(array1, array2) {
-  let result = []
-  let i = 0
-  let j = 0
 
-  while(i < array1.length && j < array2.length) {
-    if(array1[i] < array2[j]) {
-      result.push(array1[i])
-      i++
-    } else {
-      result.push(array2[j])
-      j++
-    }
-  }
-
-  while(i < array1.length) {
-    result.push(array1[i])
-    i++
-  }
-  while(j < array2.length) {
-    result.push(array2[j])
-    j++
-  }
-  
-  return result
-}
 // console.log(practiceMergeArrays([5, 6, 7, 8], [1, 2, 3, 4]))
-// -------------------------------------------------------------
 
 function mergeSort(array) {
   // 1) Break up the entire array into halves until you have arrays that are empty or have one element.
@@ -384,16 +358,48 @@ function mergeSort(array) {
 
   return mergeArrays(left, right)
 }
+// console.log(mergeSort([ 7, 8, 1, 2, 6, 3, 2, 4]))
+
+// -------------------------------------------------------------
+
+function practiceMergeArrays(array1, array2) {
+  let result = []
+  let i = 0
+  let j = 0
+
+  while (i < array1.length && j < array2.length) {
+    if (array1[i] < array2[j]) {
+      result.push(array1[i])
+      i++
+    } else {
+      result.push(array2[j])
+      j++
+    }
+  }
+
+  while (i < array1.length) {
+    result.push(array1[i])
+    i++
+  }
+  
+  while (j < array2.length) {
+    result.push(array2[j])
+    j++
+  }
+  return result
+}
 
 function practiceMergeSort(array) {
-  if(array.length <= 1) return array
-  let mid = Math.floor(array.length/2)
+  if (array.length === 1 ) return array
+
+  let mid = Math.floor(array.length / 2)
   let left = practiceMergeSort(array.slice(0, mid))
   let right = practiceMergeSort(array.slice(mid))
 
   return practiceMergeArrays(left, right)
+  
 }
-// console.log(practiceMergeSort([ 7, 8, 1, 2, 6, 3, 2, 4]))
+console.log(practiceMergeSort([ 7, 8, 1, 2, 6, 3, 2, 4]))
 
 // -------------------------------------------------------------
 
@@ -419,8 +425,6 @@ function pivotHelper(array, start=0, end) {
   swap(array, start, swapIdx)
   return swapIdx
 }
-
-// -------------------------------------------------------------
 
 // Pseudocode
 // Call the pivot on the array.
@@ -461,14 +465,6 @@ function practicePivotHelper(array, start=0, end) {
 
 function practiceQuickSort(array, left=0, right=array.length-1) {
 
-  if( left < right) {
-    let pivot = practicePivotHelper(array, left, right)
-    // left
-    practiceQuickSort(array, left, pivot - 1)
-    // right
-    practiceQuickSort(array, pivot+1, right)
-  }
- return array
 }
 
 console.log(practiceQuickSort([3, 2, 5, 9, 7, 4]))
