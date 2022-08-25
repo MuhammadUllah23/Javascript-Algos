b629cfdadafbc32eb313cb6227d4941e1db6a633
Muhammad Ullah <78986038+MuhammadUllah23@users.noreply.github.com>
2022-08-25 01:36:19 +0000

    Practice Merge Sorting

--- index.js
+++ index.js
@@ -364,11 +364,11 @@ function mergeSort(array) {
 
 function practiceMergeArrays(array1, array2) {
   let result = []
-  let i = 0
+  let i =0
   let j = 0
 
-  while (i < array1.length && j < array2.length) {
-    if (array1[i] < array2[j]) {
+ while (i < array1.length && j < array2.length) {
+    if(array1[i]< array2[j]) {
       result.push(array1[i])
       i++
     } else {
@@ -377,27 +377,27 @@ function practiceMergeArrays(array1, array2) {
     }
   }
 
-  while (i < array1.length) {
+  while(i < array1.length){
     result.push(array1[i])
     i++
   }
-  
-  while (j < array2.length) {
+  while(j < array2.length){
     result.push(array2[j])
     j++
   }
+
   return result
+  
 }
 
 function practiceMergeSort(array) {
-  if (array.length === 1 ) return array
-
-  let mid = Math.floor(array.length / 2)
+  if (array.length <= 1) return array;
+  let mid = Math.floor(array.length/2)
   let left = practiceMergeSort(array.slice(0, mid))
+
   let right = practiceMergeSort(array.slice(mid))
 
   return practiceMergeArrays(left, right)
-  
 }
 console.log(practiceMergeSort([ 7, 8, 1, 2, 6, 3, 2, 4]))
 
